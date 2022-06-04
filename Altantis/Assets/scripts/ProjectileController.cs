c116d1a43ad8405b3219dab7da846f555c8cc09c
Damocles <104439141+ShotgunOdy@users.noreply.github.com>
2022-06-04 22:08:12 +0200

    some 2

--- Altantis/Assets/scripts/ProjectileController.cs
+++ Altantis/Assets/scripts/ProjectileController.cs
@@ -10,10 +10,10 @@ public class ProjectileController : MonoBehaviour
     }
 
     private void OnCollisionEnter(Collision collision)
-    {
+   {
         if (collision.gameObject.tag == "Enemy")
-        {
-            collision.gameObject.GetComponent<Enemy_Health>().currentHealth -= 30;
+       {
+           collision.gameObject.GetComponent<Enemy_Health>().currentHealth -= 30;
             Destroy(this.gameObject);
         }
     }
