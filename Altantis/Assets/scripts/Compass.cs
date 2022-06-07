782ff79fa0cbf5c5f644b1d99d4e3ba3e8bff712
stayalivexd <96050617+stayalivexd@users.noreply.github.com>
2022-06-07 11:41:11 +0300

    stuff

--- Altantis/Assets/scripts/Compass.cs
+++ Altantis/Assets/scripts/Compass.cs
@@ -6,11 +6,54 @@ using UnityEngine.UI;
 
 public class Compass : MonoBehaviour
 {
+    public GameObject iconPrefab;
+    List<QuestMarker> questMarkers = new List<QuestMarker>();
+    
     public RawImage compassImage;
     public Transform player;
+
+    float compassUnit;
+
+    public QuestMarker one;
+    public QuestMarker two;
+    public QuestMarker three;
+
+    private void Start()
+    {
+        compassUnit = compassImage.rectTransform.rect.width / 360f;
+
+        AddQuestMarker(one);
+        AddQuestMarker(two);
+        AddQuestMarker(three);
+    }
 
     private void Update()
     {
         compassImage.uvRect = new Rect(player.localEulerAngles.y / 360f, 0f, 1f, 1f);
+
+        foreach (QuestMarker marker in questMarkers)
+        {
+            marker.image.rectTransform.anchoredPosition = GetPosOnCompass(marker);
+        }
     }
+
+    public void AddQuestMarker (QuestMarker marker)
+    {
+        GameObject newMarker = Instantiate(iconPrefab, compassImage.transform);
+        marker.image = newMarker.GetComponent<Image>();
+        marker.image.sprite = marker.icon;
+
+        questMarkers.Add(marker);
+    }
+
+    Vector2 GetPosOnCompass (QuestMarker marker)
+    {
+        Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.z);
+        Vector2 playerFwd = new Vector2(player.transform.position.x, player.transform.forward.z);
+
+        float angle = Vector2.SignedAngle(marker.position - playerPos, playerFwd);
+
+        return new Vector2(compassUnit * angle, 0f); 
+    }
+
 }
