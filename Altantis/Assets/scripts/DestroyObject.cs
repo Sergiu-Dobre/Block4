d4253696bbf96f0d6ce032f96456f6d91dbf8fd8
Sergiu Dobre <serhio107@gmail.com>
2022-06-16 12:31:25 +0200

    yesa'

--- Altantis/Assets/scripts/DestroyObject.cs
+++ Altantis/Assets/scripts/DestroyObject.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DestroyObject : MonoBehaviour
 {
     public GameObject Questmarker;
     public GameObject Questicon;
+    public int count;
 
     [SerializeField] private Compass comppasSscrip;
     [SerializeField] private QuestMarker questScript;
 
+    private void Start()
+    {
+        count = 0;
+    }
+
+    private void Update()
+    {
+      
+
+    }
     void OnTriggerEnter(Collider other)
     {
+        count += 1;
         if (other.gameObject.tag == "Player") 
         {
+           
             // destroy this object.
             comppasSscrip.completeQuest(questScript.idQuest);
             //  Destroy(Questmarker);
@@ -21,6 +35,12 @@ public class DestroyObject : MonoBehaviour
             Questicon.SetActive(false);
             Questmarker.SetActive(false);
 
+            if (count >= 4 )
+            {
+                SceneManager.LoadScene("MainMenu");
+                Debug.Log("done");
+            }
         }
+        
     }
 }
